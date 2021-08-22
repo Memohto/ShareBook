b82b2e36e3ee2ab68bffeb772bd9c786027079cd
Guillermo Tanamachi <A01631327@itesm.mx>
2021-08-22 14:19:15 -0500

    Template para los componentes

---
+++ src/components/ComponentTemplate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    this.username = "Memo";
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
