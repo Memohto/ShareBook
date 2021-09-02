db4268aaa8ab9b2cb9f06a47f47ea61cb336a32b
angela7744 <58756436+angela7744@users.noreply.github.com>
2021-09-02 11:48:09 -0500

    agregue los logos

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <Login msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+    <Login msg="Welcome to Your Vue.js App"/> 
   </div>
 </template>
 
 <script>
-import Login from '@/components/Login.vue'
 
+import Login  from '@/components/Test.vue'
 export default {
   name: 'App',
   components: {
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="Test">
+        <h1>TESTEANDO </h1> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Test",
+    data(){
+        return{
+            subtitulo: "Soy subtitulo"
+        }
+    }
+
+}
+</script>
