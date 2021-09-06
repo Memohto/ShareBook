6cdfd7d6407a9dea69a546a25a23789849b67420
jorgesalgado0999 <A01636629@itesm.mx>
2021-09-06 17:49:47 -0500

    test with home file

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <div>
+        <div>
+      <b-navbar type="light" variant="warning">
+        <button @click="signOut">Sign Out</button>
+      </b-navbar>
+    </div>
+    Contenido Principal
+  </div>
+</template>
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+export default {
+  data(){
+    return{
+    }
+  },
+  methods: {
+    async signOut(){
+      try{
+        const data = await firebase.auth().signOut();
+        console.log(data);
+        this.$router.replace({name: "login"});
+      }catch(err){
+        console.log(err);
+      } 
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
