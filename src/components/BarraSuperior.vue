8527acb2ed19d8d6052f6017c383940e1c2c47b1
angela7744 <58756436+angela7744@users.noreply.github.com>
2021-09-06 17:54:19 -0500

    ActualicÂe mi perfil y agregue assets

--- src/components/BarraSuperior.vue
+++ src/components/BarraSuperior.vue
@@ -5,22 +5,29 @@
         <a href="/"><img class="mx-4" src="./assets/logo.png" width="200" alt="an image of borregos" /></a>
       </b-navbar>
     </div>
-    <Login msg="Welcome to Your Vue.js App"/>  
+    <!-- <Login msg="Welcome to Your Vue.js App"/>  -->
     <router-view/>
   </div>
 </template>
 
 <script>
-
-  import Login  from '@/components/MiPerfil.vue'
 export default {
-  name: 'App',
-  components: {
-    Login
+  name: 'Barra',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    this.username = "Memo";
   }
 }
 </script>
 
-<style>
-  
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
 </style>
---
+++ src/components/MisLibros.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="btn-group col-3 ">
+        <a href="#" class="btn btn-warning btn-lg active" aria-current="page">Mis Libros</a>
+        <a href="#" class="btn btn-warning btn-lg">Mis Intercambios</a>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'MisLibros',
+    data() {
+        return {
+            subtitulo:"Subtitulo"
+        }
+    }
+} 
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
