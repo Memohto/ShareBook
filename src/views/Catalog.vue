6fc6397c21175569fd7001e75af1392820044362
angela7744 <58756436+angela7744@users.noreply.github.com>
2021-09-10 14:08:45 -0500

    Interdaz Catalogo

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,7 +1,61 @@
 <template>
-  <b-container>
-    Contenido Principal
-  </b-container>
+  <div>
+      <section id="creditSection">
+        <img id="credit-image" src="../assets/coin.png" alt="profile image" />
+        <body id="credit-data">
+          <p class="resaltado">Créditos</p>
+          <p id="creditAmount">625</p>
+        </body>
+      </section>
+      <section id="search">
+        <nav class="navbar navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand">Buscar Libro</a>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Titulo/Autor" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+      </section>
+  
+  <section id="myBooksSection">
+        <b-tabs class="tab" content-class="mt-3">
+            <b-tab title="Añadidos Recientemente" active>
+                <span v-for="i of [1,2,3,4,5,6,7]" :key="i">
+                    <div class="card" style="width: 21rem;">
+                        <img src="../assets/libro1.png" class="card-img-top" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Cien años de soledad</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Gabriel García Márquez</h6>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Creditos:  200</li>
+                            <li class="list-group-item">Condición:  Usado</li>
+                            <li class="list-group-item">Subido:  06/10/2021</li>
+                        </ul>
+                    </div>
+                </span>
+            </b-tab>
+            <b-tab title="Recomendaciones" active>
+                <span v-for="i of [1,2,3,4,5,6,7]" :key="i">
+                    <div class="card" style="width: 21rem;">
+                        <img src="../assets/libro2.jpeg" class="card-img-top" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Las Batallas en el desierto </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">José Emilio Pacheco</h6>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Creditos:  200</li>
+                            <li class="list-group-item">Condición:  Usado</li>
+                            <li class="list-group-item">Subido:  06/10/2021</li>
+                        </ul>
+                    </div>
+                </span>
+            </b-tab>
+        </b-tabs>
+    </section>
+    </div>
 </template>
 
 <script>
@@ -16,6 +70,49 @@ export default {
   }
 }
 </script>
+
 <style scoped>
-  
+
+#search{
+  position: absolute;
+  top: 90px;
+  width: 80%;
+  margin-right: 10px;
+  margin-left: 30px;
+}
+
+#creditSection {
+  position: absolute;
+  left:75%;
+  top: 50px;
+  width: 43%;
+}
+
+#credit-image {
+  width: 50px;
+  border-radius: 100px;
+  float: left;
+  margin-top: 40px;
+  margin-right: 10px;
+  margin-left: 180px;
+}
+#credit-data {
+  position: absolute;
+  left: 250px;
+  top: 30px;
+  width: 50%;
+  height: 80%;
+}
+#myBooksSection {
+  position: absolute;
+  top: 150px;
+  left: 2%;
+  width: 96%;
+  height: 70%;
+}
+
+.card {
+    display:inline-block;
+    
+}
 </style>
---
+++ src/views/MiPerfil.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+    <section id="fotoSection">
+      <img
+        id="perfil-image"
+        src="../assets/fotoEjemplo.jpg"
+        alt="profile image"
+      />
+      <body id="perfil-data">
+        <p id="Nombre" p class="resaltado">María Hinojosa</p>
+        <button id="edit" button type="button" class="btn btn-dark">Dark</button>
+        <br>
+        <br>
+        <br>
+        <p>I love books, cats and travel</p>
+      </body>
+    </section>
+
+    <section id="creditSection">
+      <img id="credit-image" src="../assets/coin.png" alt="profile image" />
+      <body id="credit-data">
+        <p class="resaltado">Créditos</p>
+        <p id="creditAmount">625</p>
+      </body>
+    </section>
+    
+    <section id="myBooksSection">
+        <b-tabs class="tab" content-class="mt-3">
+            <b-tab title="Mis libros" active>
+                <span v-for="i of [1,2,3,4,5,6,7]" :key="i">
+                    <div class="card" style="width: 21rem;">
+                        <img src="../assets/libro1.png" class="card-img-top" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Cien años de soledad</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Gabriel García Márquez</h6>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Creditos:  200</li>
+                            <li class="list-group-item">Condición:  Usado</li>
+                            <li class="list-group-item">Subido:  06/10/2021</li>
+                        </ul>
+                    </div>
+                </span>
+            </b-tab>
+            <b-tab title="Mis intercambios"><p>Mis intercambios</p></b-tab>
+        </b-tabs>
+    </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MiPerfil",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {
+    this.username = "Memo";
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#perfil-image {
+  width: 200px;
+  height: 200px;
+  border-radius: 100px;
+  float: left;
+  margin-right: 70px;
+}
+#Nombre {
+  
+  float: left;
+  
+}
+#edit{
+    float: left;
+    background: url("../assets/editIcon.png") no-repeat;
+    background-size: 25px;
+    width: 27px;
+    height: 27px;
+    text-indent: -9999px;
+    margin-top: 12px;
+    margin-right: 10px;
+    margin-left: 10px;
+}
+.resaltado {
+  font-weight: bold;
+  font-size: xx-large;
+}
+.notas {
+  font-size: x-small;
+}
+.notasResaltado {
+  font-weight: bold;
+  font-size: medium;
+}
+#fotoSection {
+  position: absolute;
+  left: 2%;
+  top: 90px;
+  width: 45%;
+}
+#perfil-data {
+  position: absolute;
+  left: 250px;
+  top: 30px;
+  width: 50%;
+  height: 80%;
+}
+
+#creditSection {
+  position: absolute;
+  left: 55%;
+  top: 90px;
+  width: 43%;
+}
+
+#credit-image {
+  width: 100px;
+  border-radius: 100px;
+  float: left;
+  margin-top: 40px;
+  margin-right: 10px;
+  margin-left: 120px;
+}
+#credit-data {
+  position: absolute;
+  left: 250px;
+  top: 30px;
+  width: 50%;
+  height: 80%;
+}
+ .card {
+    display:inline-block;
+    
+ 
+ }
+#creditAmount {
+  font-size: xxx-large;
+  position: absolute;
+  left: 25px;
+}
+.card-img-top {
+    width: 100%;
+    height: 15vw;
+    object-fit: cover;
+}
+#myBooksSection {
+  position: absolute;
+  top: 350px;
+  left: 2%;
+  width: 96%;
+  height: 45%;
+}
+
+.tab {
+  font-size: large;
+}
+.bookCard {
+  height: 300px;
+  width: 50%;
+  display:inline-block;
+}
+.coinCard {
+  width: 40px;
+  position: relative;
+  left: 550px;
+  top: -305px;
+}
+
+
+</style>
